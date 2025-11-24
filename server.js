fae04c7d08b8f46778233d17f1a8314a406e94ad
vinip1250-art <vinip1250@gmail.com>
2025-11-23 22:32:02 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+{
+  "name": "brazuca-direct-pro",
+  "version": "11.0.0",
+  "description": "Brazuca Addon for Render",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "axios": "^1.6.2",
+    "stremio-addon-sdk": "^1.6.10"
+  }
+}
